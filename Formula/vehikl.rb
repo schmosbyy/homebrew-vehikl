9fc809f5a7fd3c9c4414b2d84c8f7f2547cebf7d
Sakaar <sakaarpoply@gmail.com>
2024-10-09 14:48:16 -0400

    Update vehikl.rb
    update docs

--- Formula/vehikl.rb
+++ Formula/vehikl.rb
@@ -18,7 +18,10 @@ class Vehikl < Formula
       To use the vehikl function, add the following line to your shell configuration file:
   
       \e[34mecho 'source #{opt_bin}/vehikl' >> ~/.bashrc || echo 'source #{opt_bin}/vehikl' >> ~/.zshrc\e[0m
+
+      Start exploring it by running :
   
+      \e[34m vehikl --help \e[0m
       \n\n
   EOS
   end
