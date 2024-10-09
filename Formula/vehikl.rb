f6ac4c6664b0246ab1f0d1464dd7e2d578d3657e
schmosbyy <sakaarpoply@gmail.com>
2024-10-09 10:57:04 -0400

    update sha256

--- Formula/vehikl.rb
+++ Formula/vehikl.rb
@@ -2,7 +2,7 @@ class Vehikl < Formula
   desc "Shell function for Vehikl"
   homepage "https://github.com/schmosbyy/homebrew-vehikl"
   url "https://github.com/schmosbyy/homebrew-vehikl/archive/refs/tags/v1.0.tar.gz"
-  sha256 "the_generated_sha256_of_the_tarball"
+  sha256 "7b4d4b7f47c2e7a08cff95dc54e64a1a5cc3afad151d354ff2539d41283ae401"
   version "1.0"
 
   def install
